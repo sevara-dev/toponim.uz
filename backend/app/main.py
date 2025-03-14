787e2d27589ca442922d1257455cbb80e89dc522
sevara-dev <sevaranout@gmail.com>
2025-03-14 21:13:01 +0500

    Fix imports in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,8 +5,11 @@ from sqlalchemy.orm import Session
 from sqlalchemy import or_
 from typing import List
 
-from . import crud, models, schemas, auth
-from .database import SessionLocal, engine
+import crud
+import models
+import schemas
+import auth
+from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
